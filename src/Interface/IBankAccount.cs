6f8bb507c26e82bd53c1ea3c8c89d8d2fb2c0d8d
Haresh-WeCreate <haresh@wecreate.app>
2021-11-16 15:28:27 +0530

    BankAccount public functions added to interface

--- src/Interface/IBankAccount.cs
+++ src/Interface/IBankAccount.cs
@@ -11,6 +11,27 @@
     {
         #region Methods
         Task<IList<BankAccountModel>> ListAsync(string accountId);
+
+        Task<BankAccountModel> GetAsync(string accountId,
+            string bankAccountId);
+
+        Task<bool> DisableAsync(string accountId,
+            string bankAccountId);
+
+        Task<BankAccountModel> AddAsync(string accountId,
+            BankAccountModel bankAccount);
+
+        Task<BankAccountModel> AddPlaidWithTokenAsync(string accountId,
+            string plaidToken);
+
+        Task<BankAccountModel> AddPlaidWithLinkAsync(string accountId,
+            string publicToken);
+
+        Task<bool> InitiateMicroDepositsAsync(string accountId,
+           string bankAccountId);
+
+        Task<bool> CompleteMicroDepositsAsync(string accountId,
+           string bankAccountId);
         #endregion Methods
     }
     #endregion Interface
