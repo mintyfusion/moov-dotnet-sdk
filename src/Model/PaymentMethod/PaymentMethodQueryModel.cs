cc11af65e167934313e144ff315843df40e4b65e
Haresh <haresh.mintyfusion@outlook.com>
2021-11-26 14:33:05 +0530

    PaymentMethod service class added

---
+++ src/Model/PaymentMethod/PaymentMethodQueryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Tutkoo.mintyfusion.Moov.Sdk.Model.PaymentMethod
+{
+    #region Namespace
+    using System.Text.Json.Serialization;
+    #endregion
+
+    #region Class
+    public class PaymentMethodQueryModel
+    {
+        [JsonPropertyName("sourceID")]
+        public string SourceID { get; set; }
+    }
+    #endregion Class
+}
---
+++ src/Service/PaymentMethodService.cs
@@ -0,0 +1,88 @@
+﻿namespace Tutkoo.mintyfusion.Moov.Sdk.Service
+{
+    #region Namespace
+    using Interface;
+    using Model.PaymentMethod;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Tutkoo.Essentials;
+    #endregion Namespace
+
+    #region Class
+    public class PaymentMethodService : IPaymentMethod
+    {
+        #region Fields
+        private readonly IClient moovClient = null;
+        #endregion Fields
+
+        #region Constructor
+        public PaymentMethodService(IClient moovClient)
+        {
+            this.moovClient = moovClient;
+        }
+        #endregion Constructor
+
+        #region Public Methods
+        /// <summary>
+        /// Get payment methods supported by an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="queryModel">Optional parameter containing sourceId</param>
+        /// <returns>List of supported PaymentMethodModel</returns>
+        public async Task<IList<PaymentMethodModel>> ListAsync(string accountId,
+            PaymentMethodQueryModel queryModel = null)
+        {
+            if (string.IsNullOrEmpty(accountId))
+                throw new ArgumentNullException(nameof(accountId));
+
+            string scope = Utility.Format(PaymentMethodScope.Read.Value(),
+                accountId);
+
+            string endpoint = Utility.Format(PaymentMethodEndpoint.List.Value(), accountId);
+
+            IDictionary<string, string> queryParams = new Dictionary<string, string>();
+
+            // Convert model to <string, string> keyvalue pair query dictionary
+            if (queryModel != null)
+                queryParams = queryModel.AsDictionary().ToDictionary(k => k.Key, k => (string)k.Value);
+
+            IList<PaymentMethodModel> paymentMethods = await moovClient.GetAsync<IList<PaymentMethodModel>>(endpoint,
+                new List<string>() { scope },
+                queryParams);
+
+            return paymentMethods;
+        }
+
+        /// <summary>
+        /// Get paymentmethod by id for an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="paymentMethodID"></param>
+        /// <returns>PaymentMethodModel</returns>
+        public async Task<PaymentMethodModel> GetAsync(string accountId,
+            string paymentMethodID)
+        {
+            if (string.IsNullOrEmpty(accountId))
+                throw new ArgumentNullException(nameof(accountId));
+
+            if (string.IsNullOrEmpty(paymentMethodID))
+                throw new ArgumentNullException(nameof(paymentMethodID));
+
+            string scope = Utility.Format(PaymentMethodScope.Read.Value(),
+                accountId);
+
+            string endpoint = Utility.Format(PaymentMethodEndpoint.Get.Value(),
+                accountId,
+                paymentMethodID);
+
+            PaymentMethodModel paymentMethod = await moovClient.GetAsync<PaymentMethodModel>(endpoint,
+                new List<string>() { scope });
+
+            return paymentMethod;
+        }
+        #endregion Public Methods
+    }
+    #endregion Class
+}
