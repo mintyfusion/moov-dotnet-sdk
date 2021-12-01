e7287298e6d29301a6a835c4b119e04f80257bf2
Haresh <haresh.mintyfusion@outlook.com>
2021-12-01 11:24:02 +0530

    WireParticipant Model fixed

--- src/Model/Institution/WireParticipant.cs
+++ src/Model/Institution/WireParticipant.cs
@@ -8,32 +8,29 @@
     #region Class
     public class WireParticipant
     {
-        public class AchParticipant
-        {
-            [JsonPropertyName("routingNumber")]
-            public string RoutingNumber { get; set; }
+        [JsonPropertyName("routingNumber")]
+        public string RoutingNumber { get; set; }
 
-            [JsonPropertyName("telegraphicName")]
-            public string TelegraphicName { get; set; }
+        [JsonPropertyName("telegraphicName")]
+        public string TelegraphicName { get; set; }
 
-            [JsonPropertyName("customerName")]
-            public string CustomerName { get; set; }
+        [JsonPropertyName("customerName")]
+        public string CustomerName { get; set; }
 
-            [JsonPropertyName("fundsTransferStatus")]
-            public string FundsTransferStatus { get; set; }
+        [JsonPropertyName("fundsTransferStatus")]
+        public string FundsTransferStatus { get; set; }
 
-            [JsonPropertyName("fundsSettlementOnlyStatus")]
-            public string FundsSettlementOnlyStatus { get; set; }
+        [JsonPropertyName("fundsSettlementOnlyStatus")]
+        public string FundsSettlementOnlyStatus { get; set; }
 
-            [JsonPropertyName("bookEntrySecuritiesTransferStatus")]
-            public string BookEntrySecuritiesTransferStatus { get; set; }
+        [JsonPropertyName("bookEntrySecuritiesTransferStatus")]
+        public string BookEntrySecuritiesTransferStatus { get; set; }
 
-            [JsonPropertyName("date")]
-            public DateTime Date { get; set; }
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
 
-            [JsonPropertyName("location")]
-            public InstitutionAddressModel Location { get; set; }
-        }
+        [JsonPropertyName("location")]
+        public InstitutionAddressModel Location { get; set; }
     }
     #endregion Class
 }
